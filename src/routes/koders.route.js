afa016ed2e997f28ea7d549962aeaafef1fcdcb9
AlbertoEng <jesyrom0825@gmail.com>
2023-08-21 22:50:11 -0600

    acomodando el orden de los router

--- src/routes/koders.route.js
+++ src/routes/koders.route.js
@@ -10,19 +10,21 @@ router.get('/', async (req, res)=>{
         data: koderList
     })
 })
-router.post('/', async (req, res)=>{
-    const newKoder = req.body;
-    await kodersUseCases.create(newKoder);
-    res.json({
-        message: 'koder created succesfully'
-    })
-})
+
 router.get('/:id', async ( req, res)=>{
     const koderId = req.params.id;
     const koder = await kodersUseCases.getById(koderId);
     return res.json({
         message: 'ok',
         data: koder
+    })
+})
+
+router.post('/', async (req, res)=>{
+    const newKoder = req.body;
+    await kodersUseCases.create(newKoder);
+    res.json({
+        message: 'koder created succesfully'
     })
 })
 
